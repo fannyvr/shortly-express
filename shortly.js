36a54532a5e32f8165d66f36b8081481aeb9f986
Fanny Vasquez <fannyvr18@gmail.com>
2016-02-09 19:18:40 -0800

    "passing all test"

--- shortly.js
+++ shortly.js
@@ -2,7 +2,7 @@ var express = require('express');
 var util = require('./lib/utility');
 var partials = require('express-partials');
 var bodyParser = require('body-parser');
-
+var session = require('express-session');
 
 var db = require('./app/config');
 var Users = require('./app/collections/users');
@@ -19,67 +19,147 @@ app.use(partials());
 // Parse JSON (uniform resource locators)
 app.use(bodyParser.json());
 // Parse forms (signup/login)
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static(__dirname + '/public'));
 
+app.use(session({
+  secret: 'javascript',
+  resave: false,
+  saveUninitialized: true
+}));
 
-app.get('/',
-function(req, res) {
-  res.render('index');
+app.get('/login', function(req, res) {
+  res.render('login');
 });
 
-app.get('/create',
-function(req, res) {
-  res.render('index');
+app.get('/signup', function(req, res) {
+  res.render('signup');
 });
 
-app.get('/links',
-function(req, res) {
-  Links.reset().fetch().then(function(links) {
-    res.send(200, links.models);
+app.get('/', authentication,
+  function(req, res) {
+    res.render('index');
   });
-});
+
+app.get('/create', authentication,
+  function(req, res) {
+    res.render('index');
+  });
+
+app.get('/logout',
+  function(req, res) {
+    req.session.destroy();
+    res.redirect('/login');
+  });
+
+app.get('/links', authentication,
+  function(req, res) {
+    Links.reset().fetch().then(function(links) {
+      res.send(200, links.models);
+    });
+  });
 
 app.post('/links',
-function(req, res) {
-  var uri = req.body.url;
+  function(req, res) {
+    var uri = req.body.url;
 
-  if (!util.isValidUrl(uri)) {
-    console.log('Not a valid url: ', uri);
-    return res.send(404);
+    if (!util.isValidUrl(uri)) {
+      console.log('Not a valid url: ', uri);
+      return res.send(404);
+    }
+
+    new Link({
+      url: uri
+    }).fetch().then(function(found) {
+      if (found) {
+        res.send(200, found.attributes);
+      } else {
+        util.getUrlTitle(uri, function(err, title) {
+          if (err) {
+            console.log('Error reading URL heading: ', err);
+            return res.send(404);
+          }
+
+          var link = new Link({
+            url: uri,
+            title: title,
+            base_url: req.headers.origin
+          });
+
+          link.save().then(function(newLink) {
+            Links.add(newLink);
+            res.send(200, newLink);
+          });
+        });
+      }
+    });
+  });
+
+/************************************************************/
+// Write your authentication routes here
+/************************************************************/
+function authentication(req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    res.redirect('/login');
   }
+}
 
-  new Link({ url: uri }).fetch().then(function(found) {
-    if (found) {
-      res.send(200, found.attributes);
+function createSession(req, res, newUser) {
+  return req.session.regenerate(function() {
+    req.session.user = newUser;
+    res.redirect('/');
+  });
+
+}
+
+app.post('/login', function(req, res) {
+
+  var username = req.body.username;
+  var password = req.body.password;
+  var user = new User({
+    username: username
+  });
+
+  user.fetch().then(function(dbUser) {
+    if (!dbUser) {
+      res.redirect('/login');
     } else {
-      util.getUrlTitle(uri, function(err, title) {
-        if (err) {
-          console.log('Error reading URL heading: ', err);
-          return res.send(404);
+      user.comparePassword(password, function(match) {
+        if (match) {
+          createSession(req, res, user);
+        } else {
+          res.redirect('/login');
         }
-
-        var link = new Link({
-          url: uri,
-          title: title,
-          base_url: req.headers.origin
-        });
-
-        link.save().then(function(newLink) {
-          Links.add(newLink);
-          res.send(200, newLink);
-        });
       });
     }
   });
 });
 
-/************************************************************/
-// Write your authentication routes here
-/************************************************************/
+app.post('/signup', function(req, res) {
+  var username = req.body.username;
+  var password = req.body.password;
+  var user = new User({
+    username: username
+  });
 
-
-
+  user.fetch().then(function(dbUser) {
+    if (!dbUser) {
+      var newUser = new User({
+        username: username,
+        password: password
+      });
+      newUser.save().then(function(saveUser) {
+        createSession(req, res, saveUser);
+      });
+    } else {
+      res.redirect('/signup');
+    }
+  });
+});
 
 /************************************************************/
 // Handle the wildcard route last - if all other routes fail
@@ -88,7 +168,9 @@ function(req, res) {
 /************************************************************/
 
 app.get('/*', function(req, res) {
-  new Link({ code: req.params[0] }).fetch().then(function(link) {
+  new Link({
+    code: req.params[0]
+  }).fetch().then(function(link) {
     if (!link) {
       res.redirect('/');
     } else {
